6f20df036ee34d1cfe83f556f2c73894b560aa4e
vikrambaghel917 <vikrambaghel917@gmail.com>
2026-07-05 00:35:59 +0530

    robot change

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,5 +1,5 @@
 import type { MetadataRoute } from "next";
-import { absoluteUrl, getSiteUrl } from "@/lib/site";
+import { absoluteUrl } from "@/lib/site";
 
 export default function robots(): MetadataRoute.Robots {
   return {
@@ -8,6 +8,5 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
     },
     sitemap: absoluteUrl("/sitemap.xml"),
-    host: getSiteUrl(),
   };
 }
